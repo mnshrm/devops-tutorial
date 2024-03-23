b2083e4a2098b625248eaffadbae3d2b8ae1506a
mnshrm <asysnsrs665@gmail.com>
2024-03-23 13:59:52 +0530

    initial code updated

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-exports.setDateOfTheWeek = () => {
+const setDateOfTheWeek = () => {
   return new Date().toLocaleDateString("en-US", { dateStyle: "long" });
 };
 
 try {
   document.getElementById("date").innerText = setDateOfTheWeek();
 } catch (err) {}
+
+module.exports = setDateOfTheWeek;
